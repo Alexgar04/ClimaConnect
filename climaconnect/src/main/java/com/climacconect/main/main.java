d4ce59026f583e30ec69fba4dbf95977e1a68a22
Alex <a.garcia.gandia@pmaria-ontinyent.org>
2023-06-28 20:02:11 +0200

    Estructura

---
+++ climaconnect/src/main/java/com/climacconect/main/main.java
@@ -0,0 +1,9 @@
+package com.climacconect.main;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Prueba");
+	}
+
+}
--- climaconnect/src/main/java/com/projecte/climaconnect/Prueba.java
+++
@@ -1,9 +0,0 @@
-package com.projecte.climaconnect;
-
-public class Prueba {
-
-	public static void main(String[] args) {
-		System.out.println("Alex");
-	}
-
-}
